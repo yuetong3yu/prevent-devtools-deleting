888c04c450879eb1c0eae5d0aabbb4dd7ef6de22
yuetong3yu <yuetong3yu@gmail.com>
2021-11-10 10:11:21 +0800

    upgit

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 import './style.css'
 
 document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+  <h1>Hi there! Try to deleting this H1 tag using your devtools!</h1>
 `
